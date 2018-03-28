04907ebc8f0e9e3f8353681ec1854b7d7ae9c41b
Not a person <anonymous@example.com>
2018-03-28 09:25:04 -0700

    Server refactor

--- server/server.go
+++ server/server.go
@@ -5,14 +5,16 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"time"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Printf("%+v\n", time.Now())
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-		fmt.Printf("Hello\n")
-	})
-
+	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
