4d61c48da1f05e56cb250397958e9592a893b3ae
zlav <zcjlavallee@gmail.com>
2018-03-28 00:14:46 -0700

    Client working

--- client/client.go
+++ client/client.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("vim-go")
+
+	url := "http://127.0.0.1:8081"
+	headers := "A"
+	client := &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: 1024,
+		},
+		Timeout: 5 * time.Second,
+	}
+
+	ticker := time.NewTicker(1 * time.Second)
+
+	for range ticker.C {
+		request, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		request.Header.Set("name", headers)
+		resp, err := client.Do(request)
+		if resp != nil {
+			defer resp.Body.Close()
+		}
+		if err != nil {
+			fmt.Printf("Error found: %+v\nResponse: %+v\n", err, resp)
+			return
+		}
+
+		fmt.Printf("Response: %+v\n\n", resp)
+		resp.Body.Close()
+	}
 }
