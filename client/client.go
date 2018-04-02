d86c2fd349d8b9b70e0bc2e9992a31d5a5ae5ddd
zlav <zcjlavallee@gmail.com>
2018-04-02 07:38:21 -0700

    Updates to client and request demo

--- client/client.go
+++ client/client.go
@@ -2,24 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"time"
 )
 
-type Reader struct {
-	read string
-	done bool
-}
+type Reader struct{}
 
-func NewReader(toRead string) *Reader {
-	return &Reader{toRead, false}
+func NewReader() *Reader {
+	return &Reader{}
 }
 
 func (r *Reader) Read(p []byte) (n int, err error) {
-	// if r.done {
-	return 0, io.EOF
-	// }
 	// fmt.Println([]byte(r.read))
 	// for i, b := range []byte(r.read) {
 	// 	p[i] = b
@@ -37,29 +29,28 @@ func main() {
 		Transport: &http.Transport{
 			MaxIdleConnsPerHost: 1024,
 		},
-		Timeout: 5 * time.Second,
+		//	Timeout: 5 * time.Second,
 	}
 
-	ticker := time.NewTicker(1 * time.Second)
-
-	for range ticker.C {
-		request, err := http.NewRequest("GET", url, NewReader("test"))
-		if err != nil {
-			panic(err)
-		}
-
-		request.Header.Set("name", headers)
-		resp, err := client.Do(request)
-		if resp != nil {
-			defer resp.Body.Close()
-		}
-
-		if err != nil {
-			fmt.Printf("Error found: %+v\nResponse: %+v\n", err, resp)
-			return
-		} else {
-			fmt.Printf("Response: %+v\n\n", resp)
-			resp.Body.Close()
-		}
+	// ticker := time.NewTicker(1 * time.Second)
+	//for range ticker.C {
+	request, err := http.NewRequest("GET", url, NewReader())
+	if err != nil {
+		panic(err)
 	}
+
+	request.Header.Set("name", headers)
+	resp, err := client.Do(request)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	if err != nil {
+		fmt.Printf("Error found: %+v\nResponse: %+v\n", err, resp)
+		return
+	} else {
+		fmt.Printf("Response: %+v\n\n", resp)
+		resp.Body.Close()
+	}
+	// }
 }
